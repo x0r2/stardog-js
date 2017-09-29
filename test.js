6577a02a4fd30873d633be4912ecebfa2977489d
x0r2 <x0r2>
2017-09-29 18:18:39 +0300

    Fix tests

--- test.js
+++ test.js
@@ -4,8 +4,9 @@ jest.setTimeout(10000);
 
 const testDb = 'test';
 
-const stardogAdmin = new Stardog({
+const stardog = new Stardog({
     endpoint: 'http://localhost:5820',
+    database: testDb,
     auth: {
         user: 'admin',
         pass: 'admin'
@@ -13,71 +14,70 @@ const stardogAdmin = new Stardog({
 });
 
 beforeAll(async () => {
-    await stardogAdmin.createDatabase({
+    await stardog.createDatabase({
         database: testDb
     });
 
-    await stardogAdmin.update({
-        database: testDb,
+    await stardog.update({
         query: 'insert data {<urn:a> <urn:b> <urn:c>}'
     });
 });
 
 afterAll(async () => {
-    await stardogAdmin.dropDatabase({
+    await stardog.dropDatabase({
         database: testDb
     });
 });
 
 describe('database', () => {
     test('create', async () => {
-        await stardogAdmin.createDatabase({
+        await stardog.createDatabase({
             database: 'create'
         });
 
-        expect(await stardogAdmin.existsDatabase({
+        expect(await stardog.existsDatabase({
             database: 'create'
         })).toBeTruthy();
 
-        await stardogAdmin.dropDatabase({
+        await stardog.dropDatabase({
             database: 'create'
         });
     });
 
     test('drop', async () => {
-        await stardogAdmin.createDatabase({
+        await stardog.createDatabase({
             database: 'drop'
         });
 
-        expect(await stardogAdmin.existsDatabase({
+        expect(await stardog.existsDatabase({
             database: 'drop'
         })).toBeTruthy();
 
-        await stardogAdmin.dropDatabase({
+        await stardog.dropDatabase({
             database: 'drop'
         });
 
-        expect(await stardogAdmin.existsDatabase({
+        expect(await stardog.existsDatabase({
             database: 'drop'
         })).toBeFalsy();
     });
 
     test('size', async () => {
-        expect(await stardogAdmin.sizeDatabase({
+        expect(await stardog.sizeDatabase({
             database: testDb
         })).toBe(1);
     });
 
     test('list', async () => {
-        expect(await stardogAdmin.listDatabases()).toContain('test');
+        expect(await stardog.listDatabases()).toContain(testDb);
     });
 
     test('exists', async () => {
-        expect(await stardogAdmin.existsDatabase({
+        expect(await stardog.existsDatabase({
             database: testDb
         })).toBeTruthy();
 
-        expect(await stardogAdmin.existsDatabase({
+        expect(await stardog.existsDatabase({
             database: testDb + '2'
         })).toBeFalsy();
     });
@@ -85,22 +85,19 @@ describe('database', () => {
 
 describe('query', () => {
     test('ask', async () => {
-        expect(await stardogAdmin.query({
-            database: testDb,
+        expect(await stardog.query({
             accept: 'text/boolean',
             query: 'ask {<urn:a> <urn:b> <urn:c>}'
         })).toBeTruthy();
 
-        expect(await stardogAdmin.query({
-            database: testDb,
+        expect(await stardog.query({
             accept: 'text/boolean',
             query: 'ask {<urn:a> <urn:b> <urn:d>}'
         })).toBeFalsy();
     });
 
     test('select', async () => {
-        expect(await stardogAdmin.query({
-            database: testDb,
+        expect(await stardog.query({
             query: 'select * where {?s ?p ?o}'
         })).toEqual({
             head: {vars: ['s', 'p', 'o']},
@@ -115,9 +112,16 @@ describe('query', () => {
     });
 
     test('construct', async () => {
-        expect(await stardogAdmin.query({
-            database: testDb,
+        expect(await stardog.query({
             query: 'construct {?s ?p ?o} where {?s ?p ?o}'
         })).toBe('\r\n<urn:a> <urn:b> <urn:c> .\r\n');
+    });
+});
+
+describe('update', () => {
+    test('insert', async () => {
+        await stardog.update({
+            query: 'insert data {<urn:a> <urn:b> <urn:c>}'
+        });
     });
 });
